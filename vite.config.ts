9b3116ef935bf6ae9531763c3bf9f2a81152e683
Jermaine McIntosh <jermainemcintosh@Jermaines-Air.attlocal.net>
2025-10-22 21:40:10 -0400

    fix(vite): add @ -> src alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   server: { port: 5173 },
   build: { outDir: 'dist' },
 })
